95a245584a573802b95ebe6882aaf0d2ad565b8c
jimmoua1 <tsheejmaus@yahoo.com>
2020-12-18 13:34:31 -0800

    fix the storeReducer, although needed to work on components

--- src/reducers/storeReducer.js
+++ src/reducers/storeReducer.js
@@ -6,35 +6,27 @@
 // 1. Adding the Parts 
 // 2. Removing the Parts
 
-const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    additionalFeatures: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
-  };
+import { ADD_FEATURE, REMOVE_FEATURE } from "../actions";
+
+const initialState = [
+  { id: 1, name: "V-6 engine", price: 1500 },
+  { id: 2, name: "Racing detail package", price: 1500 },
+  { id: 3, name: "Premium sound system", price: 500 },
+  { id: 4, name: "Rear spoiler", price: 250 },
+];
 
 // Of course Reducer of the Two Actions
-  const storeReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case ADD_FEATURE:
-        return state.filter((feature) => feature.id !== action.payload.id);
-      case REMOVE_FEATURE:
-        return [...state, action.payload];
-      default:
-        return state;
-    }
+const storeReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_FEATURE:
+      return state.filter((feature) => feature.id !== action.payload.id);
+    case REMOVE_FEATURE:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
 };
-  
+
 export default storeReducer;
+
   
