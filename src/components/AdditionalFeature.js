bd5ff8d550a49b623f1244e7c16ba2f506f882b9
jimmoua1 <tsheejmaus@yahoo.com>
2020-12-18 13:40:11 -0800

    added a onClick from Components and passing components through props

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
-const AdditionalFeature = props => {
+const AdditionalFeature = (props) => {
+  const addFeatureHandler = (feature) => {
+    feature.preventDefault();
+    props.addFeature(props.feature);
+    props.addFeaturePrice(props.feature.price);
+  };
+
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button onClick={addFeatureHandler} className="button">
+        Add
+      </button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -2,13 +2,18 @@ import React from 'react';
 import AdditionalFeature from './AdditionalFeature';
 
 const AdditionalFeatures = props => {
+
   return (
     <div className="content">
       <h4>Additional Features</h4>
-      {props.additionalFeatures.length ? (
+      {props.store.length ? (
         <ol type="1">
-          {props.additionalFeatures.map(item => (
-            <AdditionalFeature key={item.id} feature={item} />
+          {props.store.map(item => (
+            <AdditionalFeature
+              key={item.id}
+              feature={item}
+              addFeatureHandler={props.addFeatureHandler}
+            />
           ))}
         </ol>
       ) : (
@@ -17,5 +22,6 @@ const AdditionalFeatures = props => {
     </div>
   );
 };
+
 
 export default AdditionalFeatures;
